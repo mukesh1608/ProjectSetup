cefd8a36166df879f9d2b677b8e7dd0336602f30
Mukesh Awadhiya <mukesh.awadhiya@taazaa.com>
2021-05-15 19:10:48 +0530

    Initial Commit

---
+++ ProjectSetup/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectSetup
+//
+//  Created by Mukesh_Taazaa on 15/05/21.
+//  Copyright © 2021 Mukesh_Taazaa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ProjectSetupTests/ProjectSetupTests.swift
@@ -0,0 +1,34 @@
+//
+//  ProjectSetupTests.swift
+//  ProjectSetupTests
+//
+//  Created by Mukesh_Taazaa on 15/05/21.
+//  Copyright © 2021 Mukesh_Taazaa. All rights reserved.
+//
+
+import XCTest
+@testable import ProjectSetup
+
+class ProjectSetupTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
